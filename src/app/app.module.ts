4cb388d97a96ccf21dc6d31fba8a2f0af29debe6
Oviedo Luciano <luciano.g.oviedo@gmail.com>
2022-09-12 09:30:04 -0300

    Header y Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Componentes/header/header.component';
+import { ButtonComponent } from './Componentes/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
