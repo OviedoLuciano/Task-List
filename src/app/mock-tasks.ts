d97140f62a69298b0b2d9723ce00714bbc27bba3
Oviedo Luciano <luciano.g.oviedo@gmail.com>
2022-09-12 09:39:29 -0300

    Mock-Task

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import { Task } from './Task'
+
+export const TASKS:Task[] = [
+{
+    id:1,
+    text: "Ordenar y limpiar el Departamento",
+    day: "Hoy a las 12:00",
+    reminder:true
+},
+{
+    id:2,
+    text: "Estudiar para Metodología",
+    day: "Hoy a las 14:00",
+    reminder:true
+},
+{
+    id:3,
+    text: "Hacer Flexiones",
+    day: "Hoy a las 16:00",
+    reminder:true
+},
+{
+    id:4,
+    text: "Jugar al Valorant",
+    day: "Hoy a las 18:00",
+    reminder:true
+},
+{
+    id:5,
+    text: "Mirar Game of Thrones",
+    day: "Hoy a las 21:00",
+    reminder:true
+}
+]
