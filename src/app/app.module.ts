1d4555fba24f328c7ccc0bad8749865781334a8a
Oviedo Luciano <luciano.g.oviedo@gmail.com>
2022-09-12 09:48:50 -0300

    Tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Componentes/header/header.component';
 import { ButtonComponent } from './Componentes/button/button.component';
+import { TasksComponent } from './Componentes/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
